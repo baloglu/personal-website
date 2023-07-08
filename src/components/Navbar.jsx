73784157b74705a1bc2488f7a3e85556e64ebe98
Chetin Baloglu <baloglu@gmail.com>
2023-07-08 15:58:39 +0100

    adding last changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ export default function Navbar({ page, setPage }){
         <nav id="navbar_container" className="bg-white border-gray-200 dark:bg-gray-900">
           <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <div className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Chetin Baloglu
+              Chet Baloglu
             </div>
 
 
